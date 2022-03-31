94a641d50319d7fb80b030ee015d59e1c0b1382f
kupermanak <kupermanak@gmail.com>
2022-03-31 17:50:57 +0000

    1st update

--- src/models.py
+++ src/models.py
@@ -17,3 +17,22 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(250), nullable=False)
+    height = db.Column(String(50)) 
+    mass = db.Column(String(50))
+    hair_color = db.Column(String(50))
+    eye_color = db.Column(String(50))
+    
+def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            # do not serialize the password, its a security breach
+        }
